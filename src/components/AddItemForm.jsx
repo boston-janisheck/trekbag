96b3c46f8ecd3b688614ac85a7f0d6e1867dfda2
Boston Janisheck <bostonjanisheck@gmail.com>
2024-10-22 21:18:29 -0500

    handleSubmit attributes added for UX

--- src/components/AddItemForm.jsx
+++ src/components/AddItemForm.jsx
@@ -1,27 +1,39 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Button from "./Button";
 
 const AddItemForm = ({ setItems }) => {
   const [itemText, setItemText] = useState("");
-  return (
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
+  const inputRef = useRef();
 
-        const newItem = {
-          id: 4,
-          name: itemText,
-          packed: false,
-        };
-        setItems((prev) => [...prev, newItem]);
-      }}
-    >
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Validate the input
+    if (!itemText) {
+      alert("Please enter an item.");
+      inputRef.current.focus();
+      return;
+    }
+
+    const newItem = {
+      id: 4,
+      name: itemText,
+      packed: false,
+    };
+    setItems((prev) => [...prev, newItem]);
+    setItemText("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
       <h2>Add an item</h2>
       <input
+        ref={inputRef}
         value={itemText}
         onChange={(e) => {
           setItemText(e.target.value);
         }}
+        autoFocus
       />
       <Button>Add to List</Button>
     </form>
